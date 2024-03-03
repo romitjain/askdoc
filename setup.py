5bf4a1ef6fc045f0bf2ba6175651f3d5f350ca96
romitjain <romit.73@gmail.com>
2024-03-03 18:02:43 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read_requirements():
 
 setup(
     name='askdoc',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=read_requirements(),
     entry_points={
